473649c22f1a40240d9d32e3a9a9605026929abd
ARUNAV ACHARYA <arunavacharya06.aa@gmail.com>
2021-02-03 23:00:16 +0530

    Updates

---
+++ precache-manifest.e7815c5e1e4d56bec5a1ff8673ac33bc.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3504a08853e21bc8f97093e3a292c796",
+    "url": "/MyRestaurant/index.html"
+  },
+  {
+    "revision": "a9f335583fd80c254012",
+    "url": "/MyRestaurant/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "cb4704bcf04a2581ca88",
+    "url": "/MyRestaurant/static/css/main.a5dea1d5.chunk.css"
+  },
+  {
+    "revision": "a9f335583fd80c254012",
+    "url": "/MyRestaurant/static/js/2.f0456874.chunk.js"
+  },
+  {
+    "revision": "cb4704bcf04a2581ca88",
+    "url": "/MyRestaurant/static/js/main.380a2dec.chunk.js"
+  },
+  {
+    "revision": "686c6878122274a454b1",
+    "url": "/MyRestaurant/static/js/runtime~main.9623ee5e.js"
+  },
+  {
+    "revision": "d6dd31e94697146d3e615dcb181e640c",
+    "url": "/MyRestaurant/static/media/card_home_1.d6dd31e9.jpg"
+  },
+  {
+    "revision": "0e6bbf23baa10a8aa4836247da616fbc",
+    "url": "/MyRestaurant/static/media/card_home_2.0e6bbf23.jpg"
+  },
+  {
+    "revision": "2c501851c47775a35d3425d8faec8775",
+    "url": "/MyRestaurant/static/media/card_home_3.2c501851.jpg"
+  },
+  {
+    "revision": "0630472dec696f50f64b570a7c733e05",
+    "url": "/MyRestaurant/static/media/home_kitchen.0630472d.jpg"
+  },
+  {
+    "revision": "614391b94502c82caaa482aea181243c",
+    "url": "/MyRestaurant/static/media/home_sitting.614391b9.jpg"
+  },
+  {
+    "revision": "ad45fbbc2d151b97efb37ac825d66b7c",
+    "url": "/MyRestaurant/static/media/home_top.ad45fbbc.jpg"
+  },
+  {
+    "revision": "a49fa35a5b3feee26ad9e9f1eca8cd3d",
+    "url": "/MyRestaurant/static/media/menu_top.a49fa35a.jpg"
+  }
+]);
